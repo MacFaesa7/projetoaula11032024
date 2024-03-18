bfa0a4c5bb2e8d27544fd97fe6975d5c6cb0acf2
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2024-03-18 20:45:09 -0300

    msg

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const TelegramBot = require('node-telegram-bot-api');
 // replace the value below with the Telegram token you receive from @BotFather
 const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
 // Matches "/echo [whatever]"
